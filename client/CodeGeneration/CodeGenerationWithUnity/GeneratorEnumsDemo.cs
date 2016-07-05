a8cda84eb982ceb1b32fcf008a349f19ca499807
Alexander Vinogradov <artfabrique@gmail.com>
2016-07-05 12:30:40 +0300

    T4 template project for state enums generation.

---
+++ client/CodeGeneration/CodeGenerationWithUnity/GeneratorEnumsDemo.cs
@@ -0,0 +1,27 @@
+﻿/*
+GENERATED CODE!
+DO NOT MODIFY!
+*/
+using UnityEngine;
+
+public static class StateManagerEnums 
+{
+    public enum Managers 
+    {
+        TestEnum,
+        TestEnum2,
+    }
+
+    public enum TestEnum
+    {
+        One,
+        Two,
+        Three,
+    }
+    public enum TestEnum2
+    {
+        One2,
+        Two2,
+        Three2,
+    }
+}
